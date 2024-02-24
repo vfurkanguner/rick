a6a0ed695a338145d9b69d290edd992da7330c36
vfurkanguner <vfurkanguner@gmail.com>
2024-02-24 11:58:59 +0300

    fix: types corrected

---
+++ src/types/character.ts
@@ -0,0 +1,26 @@
+type CharacterOrigin = {
+  name: string;
+  url: string;
+};
+
+type CharacterLocation = {
+  name: string;
+  url: string;
+};
+
+export type Character = {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: CharacterOrigin;
+  location: CharacterLocation;
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+  isChecked?: boolean;
+};
+

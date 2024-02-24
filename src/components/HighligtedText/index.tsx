af7d391e7a635f883a2d979d8919cca3e1085ad1
vfurkanguner <vfurkanguner@gmail.com>
2024-02-24 12:01:44 +0300

    feat: highlighted text component

---
+++ src/components/HighligtedText/index.tsx
@@ -0,0 +1,27 @@
+type Props = {
+  text: string;
+  searchTerm: string;
+};
+
+export default function HighligtedText({ text, searchTerm }: Props) {
+  if (!searchTerm.trim()) {
+    return <span>{text}</span>;
+  }
+
+  const regex = new RegExp(`(${searchTerm})`, "gi");
+  const parts = text.split(regex);
+
+  return (
+    <span className="combobox-search-item-heading">
+      {parts.map((part, index) =>
+        regex.test(part) ? (
+          <strong style={{ fontWeight: 800 }} key={index}>
+            {part}
+          </strong>
+        ) : (
+          part
+        )
+      )}
+    </span>
+  );
+}
